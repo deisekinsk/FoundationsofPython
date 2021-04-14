16da9c6f2097112cfab3789d4de838f5a9f6c0f6
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-04-14 18:22:08 -0300

    Create while.py

---
+++ april_2021/while.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+x = 0
+while (x <= 3) and (x!=3) :
+    print("Entra:{}".format(x))
+    print(("x recebe {} e soma com 1:".format(x)))
+    x = x + 1
+    print ("Resultado: {}".format(x)+"\n")
+        
+print("Final.")
