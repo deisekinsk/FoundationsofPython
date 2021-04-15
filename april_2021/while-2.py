e19f75e5c76c37c8f6a90afc3e84962a96b910ca
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-04-14 23:46:34 -0300

    Create while-2.py

---
+++ april_2021/while-2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#Estudo de laço de repetição
+a = 6
+b = 5
+c = 1
+x = a
+
+#!/usr/bin/env python
+#x = 0
+while c < b :
+    print("Entra o novo valor de x: {}".format(x))
+    print(("x recebe {} e multiplica por 'a=6' :".format(x)))
+    x = x*a
+    c = c + 1
+    print ("Resultado do while: {}".format(x)+"\n")
+    continue
+        
+print("Final.")
